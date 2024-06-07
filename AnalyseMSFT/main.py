6644ec5dcebb0eb20cb6a7eaede039e65c2e573e
AlexSchunert <a.schunert@gmail.com>
2024-06-07 15:06:32 +0200

    Added function to select data from specified timeframe, restructured data handling, not working yet

--- AnalyseMSFT/main.py
+++ AnalyseMSFT/main.py
@@ -4,14 +4,21 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics.pairwise import rbf_kernel
 
+
 # from time import mktime
 # from datetime import datetime as dt
+
+def select_data_time(data_full, start_date, end_date):
+    idx = (data_full['Date'] >= start_date) & (data_full['Date'] <= end_date)
+    data_subset = data_full[idx]
+    return data_subset
 
 
 # Parameters
 test_data_size = 0.5
 rbf_gamma = 0.005
 sigma_price = 0.01
+target_price = "High"
 
 # Load data
 raw_data = pd.read_csv("../DataSets/MSFT.csv")
@@ -19,17 +26,26 @@ raw_data = pd.read_csv("../DataSets/MSFT.csv")
 time = pd.to_datetime(raw_data["Date"], format="%Y-%m-%d")
 # Convert timestamps to days from start
 delta_time = (time - time[0]).dt.days
+# Add column delta_time to raw_data
+raw_data["dt"] = delta_time
 
 # Create data to fit
-data = pd.DataFrame({
-    "dt": delta_time,
-    "price": raw_data["High"]
-})
+# data = pd.DataFrame({
+#    "Date": raw_data["Date"],
+#    "dt": delta_time,
+#    "price": raw_data["High"]
+# })
 
 # Split into train- and test-data
+data = select_data_time(raw_data, "2000-01-01", "2005-12-31")
+data = data[["Date", "dt", target_price]]
+
 train_data, test_data = train_test_split(data, test_size=test_data_size, random_state=42)
 train_data = train_data.sort_index()
 test_data = test_data.sort_index()
+
+# train_data = select_data_time(train_data, "2000-01-01", "2005-12-31")
+# test_data = select_data_time(test_data, "2000-01-01", "2005-12-31")
 
 # Compute kernel for data points => K_xx
 k_xx = rbf_kernel(train_data['dt'].values.reshape(-1, 1), train_data['dt'].values.reshape(-1, 1), gamma=rbf_gamma)
@@ -40,7 +56,7 @@ L = np.linalg.cholesky(k_xx + sigma_price ** 2 * np.eye(k_xx.shape[0]))
 predictive_cov = np.linalg.inv(L.T).dot(np.linalg.inv(L))
 
 # Compute representer weights => zero prior
-alpha = predictive_cov @ train_data["price"].values.reshape(-1, 1)
+alpha = predictive_cov @ train_data[target_price].values.reshape(-1, 1)
 
 # predict for all data
 k_zx = rbf_kernel(data['dt'].values.reshape(-1, 1), train_data['dt'].values.reshape(-1, 1), gamma=rbf_gamma)
@@ -48,16 +64,14 @@ price_predicted = k_zx @ alpha
 
 # Create result series
 result = pd.DataFrame({
-    "dt" : delta_time,
+    "dt": data["dt"],
     "price_prediction": pd.Series(price_predicted.reshape(-1))
 })
 
-
-plt.plot(train_data["dt"],train_data["price"],'.')
-plt.plot(test_data["dt"],test_data["price"],'r.')
-plt.plot(result["dt"],result["price_prediction"],'g')
+plt.plot(train_data["dt"], train_data[target_price], '.')
+plt.plot(test_data["dt"], test_data[target_price], 'r.')
+plt.plot(result["dt"], result["price_prediction"], 'g')
 plt.show()
-
 
 # Split into train and test data
 # delta_time_train
