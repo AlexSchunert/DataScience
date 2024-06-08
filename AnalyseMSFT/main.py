8c019386ef41f3be7a8d22865aaf4a414d212483
AlexSchunert <a.schunert@gmail.com>
2024-06-08 13:05:59 +0200

    Changed plot symbol+color for train_data in plot_predictionresult, adapted parameters to fit return series

--- AnalyseMSFT/main.py
+++ AnalyseMSFT/main.py
@@ -3,15 +3,15 @@ from analyses import fit_gp_standard
 
 # Parameters
 test_data_size = 0.5
-rbf_length_scale = 1.0
-rbf_output_scale = 10.0
+rbf_length_scale = 5.0
+rbf_output_scale = 20.0
 sigma_price = 0.2  # Make sigma_price a function of time?
-sigma_return = 5.0
+sigma_return = 1.0
 target_price = "High"
 use_return = True
 plot_shading_mode = "2-sigma"
 start_date = "1990-01-01"
-end_date = "1990-12-31"
+end_date = "1995-12-31"
 
 # Load dataset
 raw_data = load_msft()
--- AnalyseMSFT/plot_tools.py
+++ AnalyseMSFT/plot_tools.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def plot_prediction_result(train_data, test_data, result, target_price, plot_shading_mode):
     plt.figure()
-    plt.plot(pd.to_datetime(train_data["Date"]), train_data[target_price], 'k+')
+    plt.plot(pd.to_datetime(train_data["Date"]), train_data[target_price], 'g*')
     plt.plot(pd.to_datetime(test_data["Date"]), test_data[target_price], 'b.')
     plt.plot(pd.to_datetime(result["Date"]), result["price_prediction"], 'g')
     plt.plot(pd.to_datetime(result["Date"]), result["price_prediction"] + result["std"], 'r--')
