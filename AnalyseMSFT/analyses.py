a6bf7db2fa171e4d43bd46966b70753e521e9593
AlexSchunert <a.schunert@gmail.com>
2024-06-08 13:18:56 +0200

    Moved standard gp fit to analyses functions

--- AnalyseMSFT/analyses.py
+++ AnalyseMSFT/analyses.py
@@ -1,6 +1,7 @@
-from utils import train_test_split, compute_kernel_matrices, construct_prediction_result
+from utils import train_test_split, compute_kernel_matrices, construct_prediction_result, compute_return, select_data_time
 from plot_tools import plot_prediction_result
 from gaussian_process import condition_gpr, predict_gpr
+
 
 def fit_gp_standard(data,
                     target_price,
@@ -9,7 +10,6 @@ def fit_gp_standard(data,
                     rbf_output_scale,
                     sigma_price,
                     plot_shading_mode):
-
     # split into training and test data
     train_data, test_data = train_test_split(data, test_data_size)
     # Compute kernel matrices
@@ -22,3 +22,46 @@ def fit_gp_standard(data,
     result = construct_prediction_result(data, price_predicted, std_prediction)
     # plot result
     plot_prediction_result(train_data, test_data, result, target_price, plot_shading_mode)
+
+
+def fit_gp_time_period_train_test_split(raw_data,
+                                        target_price,
+                                        test_data_size,
+                                        use_return,
+                                        start_date,
+                                        end_date,
+                                        rbf_length_scale,
+                                        rbf_output_scale,
+                                        sigma_price,
+                                        sigma_return,
+                                        plot_shading_mode):
+    # In case the target quantity is the Return, compute it
+    if use_return:
+        raw_data = compute_return(raw_data, target_price)
+
+    # Split into train- and test-data
+    data = select_data_time(raw_data, start_date, end_date)
+
+    if not use_return:
+        # Remove unnecessary colums from data
+        data = data[["Date", "dt", target_price]]
+        # Fit gp
+        fit_gp_standard(data,
+                        target_price,
+                        test_data_size,
+                        rbf_length_scale,
+                        rbf_output_scale,
+                        sigma_price,
+                        plot_shading_mode)
+
+    else:
+        # Remove unnecessary colums from data
+        data = data[["Date", "dt", "Return"]]
+        # Fit gp
+        fit_gp_standard(data,
+                        "Return",
+                        test_data_size,
+                        rbf_length_scale,
+                        rbf_output_scale,
+                        sigma_return,
+                        plot_shading_mode)
--- AnalyseMSFT/main.py
+++ AnalyseMSFT/main.py
@@ -1,5 +1,5 @@
-from utils import load_msft, select_data_time, compute_return
-from analyses import fit_gp_standard
+from utils import load_msft
+from analyses import fit_gp_time_period_train_test_split
 
 # Parameters
 test_data_size = 0.5
@@ -16,35 +16,15 @@ end_date = "1995-12-31"
 # Load dataset
 raw_data = load_msft()
 
-# In case the target quantity is the Return, compute it
-if use_return:
-    raw_data = compute_return(raw_data, target_price)
-
-# Split into train- and test-data
-data = select_data_time(raw_data, start_date, end_date)
-
-if not use_return:
-    # Remove unnecessary colums from data
-    data = data[["Date", "dt", target_price]]
-    # Fit gp
-    fit_gp_standard(data,
-                    target_price,
-                    test_data_size,
-                    rbf_length_scale,
-                    rbf_output_scale,
-                    sigma_price,
-                    plot_shading_mode)
-
-else:
-    # Remove unnecessary colums from data
-    data = data[["Date", "dt", "Return"]]
-    # Fit gp
-    fit_gp_standard(data,
-                    "Return",
-                    test_data_size,
-                    rbf_length_scale,
-                    rbf_output_scale,
-                    sigma_return,
-                    plot_shading_mode)
-
+fit_gp_time_period_train_test_split(raw_data,
+                                    target_price,
+                                    test_data_size,
+                                    use_return,
+                                    start_date,
+                                    end_date,
+                                    rbf_length_scale,
+                                    rbf_output_scale,
+                                    sigma_price,
+                                    sigma_return,
+                                    plot_shading_mode)
 pass
