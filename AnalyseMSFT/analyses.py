1f8bfa0b2de830a6edd365df05938fc3c5cde6c4
AlexSchunert <144051949+AlexSchunert@users.noreply.github.com>
2024-06-09 12:58:33 +0200

    Refactor/ref analyses and generalize gp code (#9)
    * Cleaned up various interfaces, made gp_predict=>gp_process main interface function for gp estimation, gp code is generalized
    
    * Corrected result labels in gp_prediction_vs_martingale
    
    * Added plot of iteration results to gp_prediction_vs_martingale, cleaned up fct gp_prediction_vs_martingale
    
    * refactoed fit_gpt to handle all prediction/interpolation cases, added Return as proper field+changed parameter handling
    
    * Generalized fit_gp interface further, integrated fit_gp in gp_prediction_vs_martingale
    
    * Cleaned up code, added ax-labels, tite, and legend to line plot
    
    * Added calculation and output of histogram mean and std in plot_prediction_error_statistic

--- AnalyseMSFT/analyses.py
+++ AnalyseMSFT/analyses.py
@@ -1,88 +1,117 @@
 import numpy as np
 from random import randint
+import pandas as pd
+from datetime import datetime, timedelta
 
 from utils import train_test_split, compute_kernel_matrices, construct_prediction_result, compute_return, \
-    select_data_time, select_data_idx
+    select_data_time
 from plot_tools import plot_prediction_result, plot_prediction_error_statistic
-from gaussian_process import condition_gpr, predict_gpr
+from gaussian_process import gp_process
 
 
-def fit_gp_standard(data,
-                    target_price,
-                    test_data_size,
-                    rbf_length_scale,
-                    rbf_output_scale,
-                    sigma_price,
-                    plot_shading_mode):
-    # split into training and test data
-    train_data, test_data = train_test_split(data, test_data_size)
-    # Compute kernel matrices
-    k_xx, k_zx, k_zz = compute_kernel_matrices(data, train_data, rbf_length_scale, rbf_output_scale, "rbf")
-    # condition on data
-    alpha, predictive_cov = condition_gpr(train_data, k_xx, target_price, sigma_price)
-    # predict for all data
-    price_predicted, std_prediction = predict_gpr(alpha, k_zx, k_zz, predictive_cov)
-    # create result
-    result = construct_prediction_result(data, price_predicted, std_prediction)
-    # plot result
-    plot_prediction_result(train_data, test_data, result, target_price, plot_shading_mode)
+def fit_gp(data,
+           parameters,
+           subsample_timeframe=False,
+           start_date=None,
+           end_date=None,
+           prediction_horizon=None,
+           prediction_horizon_mode="days",
+           prediction_mode="all",
+           plot_results=True):
 
+    # Check inputs
+    if start_date is None:
+        start_date = parameters.start_date
+    if end_date is None:
+        end_date = parameters.end_date
+    if prediction_horizon is None:
+        prediction_horizon = parameters.prediction_horizon
 
-def fit_gp_time_period_train_test_split(raw_data, parameters):
-    # In case the target quantity is the Return, compute it
-    if parameters.use_return:
-        raw_data = compute_return(raw_data, parameters.target_price)
+    # Set name of result
+    result_label = parameters.target_label
 
-    # Split into train- and test-data
-    data = select_data_time(raw_data, parameters.start_date, parameters.end_date)
+    # Get rid of unnecessary columns
+    data = data[["Date", "dt", parameters.target_label]]
 
-    if not parameters.use_return:
-        # Remove unnecessary colums from data
-        data = data[["Date", "dt", parameters.target_price]]
-        # Fit gp
-        fit_gp_standard(data,
-                        parameters.target_price,
-                        parameters.test_data_size,
-                        parameters.rbf_length_scale,
-                        parameters.rbf_output_scale,
-                        parameters.sigma_price,
-                        parameters.plot_shading_mode)
+    # Get selected timeframe
+    data_timeframe = select_data_time(data, start_date, end_date)
+
+    # split into training and test data if subsampling is enabled
+    if subsample_timeframe:
+        data_timeframe_train, data_timeframe_test = train_test_split(data_timeframe, parameters.test_data_size)
+    else:
+        data_timeframe_train = data_timeframe
+        data_timeframe_test = pd.DataFrame(columns=data_timeframe_train.columns)
+
+    # Construct prediction data
+    if prediction_mode == "all":
+        data_timeframe_test = data_timeframe
+    elif prediction_mode == "predict_only":
+        data_timeframe_test = pd.DataFrame(columns=data_timeframe_train.columns)
+    else:
+        data_timeframe_test = data_timeframe
+
+    if prediction_horizon_mode == "index":
+        idx_date_larger = np.where(data["Date"] > end_date)
+        if idx_date_larger is None:
+            data_timeframe_predict = pd.DataFrame(columns=data_timeframe_train.columns)
+        elif np.where((data["Date"] >= end_date))[0].shape[0] == 0:
+            data_timeframe_predict = pd.DataFrame(columns=data_timeframe_train.columns)
+        else:
+            idx_start_prediction = np.min(idx_date_larger[0])
+            idx_end_prediction = np.min([idx_start_prediction + prediction_horizon - 1, data.shape[0]])
+            data_timeframe_predict = data.loc[idx_start_prediction:idx_end_prediction]
 
     else:
-        # Remove unnecessary colums from data
-        data = data[["Date", "dt", "Return"]]
-        # Fit gp
-        fit_gp_standard(data,
-                        "Return",
-                        parameters.test_data_size,
+        # Select and concatenate with data_timeframe_test
+        start_date_prediction = (datetime.strptime(parameters.end_date, "%Y-%m-%d") + timedelta(1)).strftime("%Y-%m-%d")
+        end_date_prediction = (
+                    datetime.strptime(parameters.end_date, "%Y-%m-%d") + timedelta(prediction_horizon)).strftime(
+            "%Y-%m-%d")
+        data_timeframe_predict = select_data_time(data, start_date_prediction, end_date_prediction)
+
+    if data_timeframe_test.empty is False or data_timeframe_predict.empty is False:
+        data_timeframe_test = pd.concat([df for df in [data_timeframe_test, data_timeframe_predict] if not df.empty]).\
+            sort_index()
+    else:
+        return
+
+    result = gp_process(data_timeframe_test,
+                        data_timeframe_train,
+                        parameters.target_label,
+                        result_label,
+                        parameters.sigma_used,
                         parameters.rbf_length_scale,
-                        parameters.rbf_output_scale,
-                        parameters.sigma_return,
-                        parameters.plot_shading_mode)
+                        parameters.rbf_output_scale)
 
-
-def gp_predict(test_data, train_data, target_quantity, sigma_measurement, rbf_length_scale, rbf_output_scale):
-    # Fit gp
-    k_xx, k_zx, k_zz = compute_kernel_matrices(test_data,
-                                               train_data,
-                                               rbf_length_scale,
-                                               rbf_output_scale,
-                                               "rbf")
-    # condition on data
-    alpha, predictive_cov = condition_gpr(train_data, k_xx, target_quantity, sigma_measurement)
-    # predict for all data
-    price_predicted, std_prediction = predict_gpr(alpha, k_zx, k_zz, predictive_cov)
-    # create result
-    result = construct_prediction_result(test_data, price_predicted, std_prediction)
+    if plot_results:
+        plot_prediction_result(data_timeframe_train,
+                               data_timeframe_test,
+                               result,
+                               parameters.target_label,
+                               result_idx=result_label,
+                               plot_shading_mode=parameters.plot_shading_mode)
 
     return result
 
 
-def gp_prediction_vs_martingale(raw_data, parameters):
+def gp_prediction_vs_martingale(raw_data, parameters, plot_iterations=False):
     # In case the target quantity is the Return, compute it
+    """
     if parameters.use_return:
         raw_data = compute_return(raw_data, parameters.target_price)
         raw_data = raw_data.reset_index(drop=True)
+        target_quantity_idx = "Return"
+        result_label = "Return"
+        sigma_target_quantity = parameters.sigma_return
+    else:
+        target_quantity_idx = parameters.target_price
+        result_label = parameters.target_price
+        sigma_target_quantity = parameters.sigma_price
+    """
+    # Set name of result and target quantity index
+    result_label = parameters.target_label
+    target_quantity_idx = parameters.target_label
 
     # create output arrays
     prediction_error = np.zeros(parameters.num_iter_error_stat, )
@@ -99,40 +128,36 @@ def gp_prediction_vs_martingale(raw_data, parameters):
 
         # Select data from randomly selected timeframe
         idx_end = randint(min_idx_end, max_idx_end)
-        train_data = raw_data.loc[idx_end - parameters.num_data_points_gp_fit + 1:idx_end]
-        test_data = raw_data.loc[[idx_end + 1]]
-        if not parameters.use_return:
-            # Remove unnecessary colums from data
-            train_data = train_data[["Date", "dt", parameters.target_price]]
-            test_data = test_data[["Date", "dt", parameters.target_price]]
-            # Fit gp
-            result = gp_predict(test_data,
-                                train_data,
-                                parameters.target_price,
-                                parameters.sigma_price,
-                                parameters.rbf_length_scale,
-                                parameters.rbf_output_scale)
+        # Determine start and end date
+        start_date = raw_data.loc[idx_end-parameters.num_data_points_gp_fit + 1]["Date"]
+        end_date = raw_data.loc[idx_end]["Date"]
 
-            # calculate prediction and store
-            prediction_error[i] = result["price_prediction"].values[0] - test_data[parameters.target_price].values[0]
-            martingale_error[i] = train_data[parameters.target_price].values[-1] - \
-                                  test_data[parameters.target_price].values[0]
+        if plot_iterations:
+            prediction_mode = "all"
 
         else:
-            # Remove unnecessary colums from data
-            train_data = train_data[["Date", "dt", "Return"]]
-            test_data = test_data[["Date", "dt", "Return"]]
+            prediction_mode = "predict_only"
 
-            result = gp_predict(test_data,
-                                train_data,
-                                "Return",
-                                parameters.sigma_return,
-                                parameters.rbf_length_scale,
-                                parameters.rbf_output_scale)
+        result = fit_gp(raw_data,
+                        parameters,
+                        subsample_timeframe=False,
+                        start_date=start_date,
+                        end_date=end_date,
+                        prediction_horizon=1,
+                        prediction_horizon_mode="index",
+                        prediction_mode=prediction_mode,
+                        plot_results=plot_iterations)
 
-            # calculate prediction and store
-            prediction_error[i] = result["price_prediction"].values[0] - test_data["Return"].values[0]
-            martingale_error[i] = -test_data["Return"].values[0]
+        # calculate prediction and store
+        if parameters.use_return:
+            prediction_error[i] = result[result_label].values[-1] - \
+                                  raw_data[target_quantity_idx][idx_end+1]
+            martingale_error[i] = -raw_data[target_quantity_idx][idx_end+1]
+        else:
+            prediction_error[i] = result[result_label].values[-1] - \
+                                  raw_data[target_quantity_idx][idx_end+1]
+            martingale_error[i] = raw_data[target_quantity_idx][idx_end] - \
+                                  raw_data[target_quantity_idx][idx_end+1]
 
     plot_prediction_error_statistic(prediction_error,
                                     reference_error=martingale_error,
--- AnalyseMSFT/plot_tools.py
+++ AnalyseMSFT/plot_tools.py
@@ -1,37 +1,72 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as pltdates
+import numpy as np
 import pandas as pd
 
 
-def plot_prediction_result(train_data, test_data, result, target_price, plot_shading_mode):
-    plt.figure()
-    plt.plot(pd.to_datetime(train_data["Date"]), train_data[target_price], 'g*')
-    plt.plot(pd.to_datetime(test_data["Date"]), test_data[target_price], 'b.')
-    plt.plot(pd.to_datetime(result["Date"]), result["price_prediction"], 'g')
-    plt.plot(pd.to_datetime(result["Date"]), result["price_prediction"] + result["std"], 'r--')
-    plt.plot(pd.to_datetime(result["Date"]), result["price_prediction"] - result["std"], 'r--')
+def plot_prediction_result(train_data,
+                           test_data,
+                           result,
+                           target_quantity_idx,
+                           result_idx="prediction",
+                           plot_shading_mode=2.0):
+    fig, ax = plt.subplots()
+    # Plot data
+    plt.plot(pd.to_datetime(test_data["Date"]), test_data[target_quantity_idx], 'b.', label="Test data")
+    plt.plot(pd.to_datetime(train_data["Date"]), train_data[target_quantity_idx], 'g*', label="Train data")
+    # Plot prediction
+    plt.plot(pd.to_datetime(result["Date"]), result[result_idx], 'g-', label="GP mean-fct.")
+    # Plot standard deviation
+    plt.plot(pd.to_datetime(result["Date"]), result[result_idx] + result["std"], 'r--')
+    plt.plot(pd.to_datetime(result["Date"]), result[result_idx] - result["std"], 'r--', label="1-sigma")
     if plot_shading_mode == "2-sigma":
-        upper_bound = result["price_prediction"] + 2.0 * result["std"]
-        lower_bound = result["price_prediction"] - 2.0 * result["std"]
+        upper_bound = result[result_idx] + 2.0 * result["std"]
+        lower_bound = result[result_idx] - 2.0 * result["std"]
         plt.fill_between(pd.to_datetime(result["Date"]), lower_bound, upper_bound, where=(upper_bound >= lower_bound),
                          interpolate=True,
-                         color='gray', alpha=0.5)
+                         color='gray', alpha=0.5, label=plot_shading_mode)
     else:
-        upper_bound = result["price_prediction"] + 2.0 * result["std"]
-        lower_bound = result["price_prediction"] - 2.0 * result["std"]
+        upper_bound = result[result_idx] + 2.0 * result["std"]
+        lower_bound = result[result_idx] - 2.0 * result["std"]
         plt.fill_between(pd.to_datetime(result["Date"]), lower_bound, upper_bound, where=(upper_bound >= lower_bound),
                          interpolate=True,
-                         color='gray', alpha=0.5)
+                         color='gray', alpha=0.5, label=plot_shading_mode)
+
+    plt.title("GP fit for quantity "+result_idx)
+    plt.xlabel("Date")
+    plt.ylabel(target_quantity_idx)
+    plt.legend(loc='upper right')
 
 
+    # Set the date format on the x-axis
+    ax.xaxis.set_major_formatter(pltdates.DateFormatter('%Y-%m-%d'))
+    ax.xaxis.set_major_locator(pltdates.DayLocator(interval=10))  # Major ticks every 10 days
+    ax.xaxis.set_minor_locator(pltdates.DayLocator())
 
     plt.show()
 
 
 def plot_prediction_error_statistic(prediction_error, reference_error=None, num_bins=50):
-    plt.figure()
-    plt.hist(prediction_error, bins=num_bins, color="green", histtype="bar", alpha=0.5, rwidth=0.8, density=True, label="Prediction error")
+
+    # Compute mean and std for error vecs
+    mean_prediction_error = np.mean(prediction_error)
+    std_prediction_error = np.std(prediction_error)
+    label_prediction_error = \
+        "Prediction error, m: " + str(round(mean_prediction_error, 2)) + ", s: " + str(round(std_prediction_error, 2))
+
     if reference_error is not None:
-        plt.hist(reference_error, bins=num_bins, color="gray", histtype="bar", alpha=0.5, rwidth=0.8, density=True, label="Reference error")
+        mean_reference_error = np.mean(reference_error)
+        std_reference_error = np.std(reference_error)
+        label_reference_error = \
+            "Reference error, m: " + str(round(mean_reference_error, 2)) + ", s: " + str(round(std_reference_error, 2))
+
+    plt.figure()
+    plt.hist(prediction_error, bins=num_bins, color="green", histtype="bar", alpha=0.5, rwidth=0.8, density=True,
+             label=label_prediction_error)
+
+    if reference_error is not None:
+        plt.hist(reference_error, bins=num_bins, color="gray", histtype="bar", alpha=0.5, rwidth=0.8, density=True,
+                 label=label_reference_error)
 
     plt.legend(loc='upper right', title='Histograms')
     plt.xlabel("error")
