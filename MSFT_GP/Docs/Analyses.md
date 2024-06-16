4689317428466edf469edc319d20c6095bc5f6ce
AlexSchunert <144051949+AlexSchunert@users.noreply.github.com>
2024-06-16 16:07:47 +0200

    Feature/analyse 1 day squared or abs return (#16)
    * Added command line option to use absolute returns, implemented calculation, strorage and handling for absolute returns => TBD: Fix interfaces of functions in main.py
    
    * Corrections to readme
    
    * Added handling of abs returns to all main.py functions
    
    * Included more data subsets in plot_return_full_subs
    
    * Added chapter 'Absolute values of one-day returns' with figures
    
    * corrected stuff in readme

--- MSFT_GP/Docs/Analyses.md
+++ MSFT_GP/Docs/Analyses.md
@@ -22,7 +22,7 @@ Notes:
 |$\sigma_{P}$: std of price   |0.01 $    |
 </div>
 
-It should be noted here that those parameters have been chosen to achieve a visually compelling result. If it's of any practical use remains to be seen.
+**Bottom line:** The parameters have been chosen to achieve a visually compelling result. If it's of any practical use remains to be seen. Implementation seems to work as result look plausible. 
 
 ### One-day returns using adjusted closing price
 Start with a plot of the timeseries of one-day returns of the adjusted closing prices
@@ -40,8 +40,8 @@ Notes:
 
 * Unfortunately, the plot suggests very small to no temporal correlation of the returns. As far as my reading goes, this aligns with empirical findings from finance and is to be expected for a somewhat efficient market. 
 * If there is no temporal correlation in the timeseries, using a gaussian process (or any other method) is pointless: 
-  * The predictive covariance becomes diagonal with identical diagonal elements => representer weights are simply the scaled observations
-  * Due to low correlation, the prediction does not even depend on the data and is simply given by the prior 
+  * The predictive covariance becomes diagonal => Representer-weights are simply the scaled observations. In case of existing temporal correlation, each representer-weight is a linear combination of observations with weights depending on the kernel function and the noise level. 
+  * If there is no temporal correlation, the prediction does not even depend on the data and is simply given by the prior 
   * "Prediction" for training data yields a result between prior and original observation depending on the assumed noise level. 
 
 Let's look at the data in more detail:
@@ -56,10 +56,10 @@ To generate the plot, run `python -m main --mode plot_return_full`
 
 Notes:
 
-* signal vs time is identical to Figure 1
+* signal vs time is identical to Figure 2
 * Autocorrelation shows the autocorrelation function of the signal using the function acovf from statsmodels.tsa.stattools to handle missing data (no trading at the weekend).
 * PSD is the power spectral density estimated using a Lomb-Scargle periodogram taken from astropy.timeseries (again to handle data gaps). The cut-off frequency is set according to the median sampling of the data. 
-* Histogram is simply the histogram of the returns
+* Histogram is simply the histogram of the returns.
 * We need to be a bit careful with the interpretation as acf- and psd- calculation require stationarity. 
   * While the mean of the data is most likely stationary, the variance appears to vary quite a bit. 
   * There seem to be clusters of higher and clusters of lower variance (=> volatility clustering?). 
@@ -68,7 +68,7 @@ Notes:
   * Autocorrelation drops immediately to zero
   * PSD is flat(ish) => white noise
 
-In order to make sure to not miss anything, it makes sense to look at some subsets of the data with constant variance. 
+Just to make sure, a detailed looks at two subsets of the data with constant variance. 
 
 To generate the plot, run `python -m main --mode plot_return_full_subs`
 <p align="center">
@@ -84,4 +84,72 @@ Notes:
 * Pretty much confirms results in Figure 3 => No autocorrelation in the data
 
 **Bottom line:** Predicting one-day returns using GPs will not work due to the lack of temporal correlation. 
+
+### Absolute values of one-day returns
+Analyze absolute values of one-day returns:
+* Volatility clustering: Large changes often follow large changes and small changes often follow small changes => implies temporal correlation in the data
+* Is it possible to predict absolute values of returns? => proxy for volatility => volatility prediction is useful for risk assessment
+* Why not squared returns?
+  * Data would follow a $\chi^2$-distribution. 
+  * For modelling with a GP later down the line, I rather have non-negative gaussian distributed data and deal with or accept the issues from having a non-negativity constraint, compared to $\chi^2$-distributed data.
+  *  Not sure if this is the best course of action. According to ChatGPT using non-negative gaussian data leads to less issues in GPs provided that non-negativity is accounted for. $\chi^2$-distributed data violates the assumption inherent to GPs that likelihood function and prior are gaussian. 
+
+To generate the plot, run `python -m main --mode plot_return_ts --return_mode abs` 
+<p align="center">
+  <img src="resources/OneDayAbsReturns_ClosingAdj_TimeseriesFull.png" alt="drawing" width="600"/>
+  <br>
+  <em>Figure 5: Absolute values of one-day returns based on adjusted closing price.</em>
+</p>
+
+To generate the plot, run `python -m main --mode plot_return_full --return_mode abs` 
+
+<p align="center">
+  <img src="resources/OneDayAbsReturns_ClosingAdj_AllPlotsFull.png" alt="drawing" width="600"/>
+  <br>
+  <em>Figure 6: Detailed look at absolute values of one-day returns based on adjusted closing price.</em>
+</p>
+
+Notes:
+* Like Figures 2 and 3 for the absolute values of the returns (adj. closing price)
+* Autocorrelation and PSD suggest that there is some slight temporal correlation 
+* Same as for Figures 2 and 3 => Variance over complete timeframe not constant => Look at subsets to have data closer to stationarity
+
+To generate the plot, run `python -m main --mode plot_return_full_subs --return_mode abs`
+<p align="center">
+  <img src="resources/OneDayAbsReturns_ClosingAdj_AllPlotsSubsLowVar.png" alt="drawing" width="400"/> &nbsp;&nbsp;&nbsp;  
+    <img src="resources/OneDayAbsReturns_ClosingAdj_AllPlotsSubsHighVar.png" alt="drawing" width="400"/>
+  <br>
+  <em>Figure 7: Detailed look at subsets of the data assumed stationary.</em>
+</p>
+
+Notes:
+* Timeframes identical to Figure 4
+* Timeframe with low variance 1st January 1993 - 31st December 1995 (left) => TFL
+* Timeframe with high variance 1st January 2000 - 31st December 2003 (right) => TFH
+* For TFH the temporal correlation is at least as "strong" as for complete timeseries (Figure 6)
+* TFL shows hardly any temporal correlation
+
+Look at two more subsets
+To generate the plot, run `python -m main --mode plot_return_full_subs --return_mode abs`
+<p align="center">
+  <img src="resources/OneDayAbsReturns_ClosingAdj_AllPlotsSubsLowVar2.png" alt="drawing" width="400"/> &nbsp;&nbsp;&nbsp;  
+    <img src="resources/OneDayAbsReturns_ClosingAdj_AllPlotsSubsHighVar2.png" alt="drawing" width="400"/>
+  <br>
+  <em>Figure 7: Detailed look at subsets of the data assumed stationary.</em>
+</p>
+
+Notes:
+* Timeframe with low variance 1st January 2011 - 31st December 2012 (left) => TFL2
+* Timeframe with high variance 1st January 2008 - 31st December 2009 (right) => TFH2
+* TFL2 similar to TFL => Hardly any temporal correlation
+* TFH2 shows quite significant temporal correlation compared to all other results
+
+**Bottom line:**
+* There seems to some temporal correlation in absolute values of returns
+* Degree of temporal correlation varies within the timeseries
+
+**Next:** 
+* Use GP to predict absolute returns
+* Estimate autocorrelation for given training data and use it to tune kernel hyperparameters
+* Kernel function?
 
--- MSFT_GP/main.py
+++ MSFT_GP/main.py
@@ -27,42 +27,64 @@ def run_initial_example():
     # Fit GP and plot
     fit_gp(raw_data, parameters, prediction_horizon_mode="day", subsample_timeframe=True, prediction_mode="all")
 
-def plot_return_ts():
+
+def plot_return_ts(return_mode="standard"):
+    """
+    Only timeseries plot of target quantity, complete timeframe
+
+    :param return_mode: "If "standard", the signed returns are used. If "abs" absolute values of returns are used"
+    :type return_mode: str
+
+    :return: ---
+    :rtype: None
+    """
     # Parameters
     parameters = Parameters(start_date="1980-01-01",
                             end_date="2024-12-31",
                             tick_interval_x=1000,
                             use_return=True,
                             prediction_horizon=10,
-                            target_label="Adj Close")
+                            target_label="Adj Close",
+                            return_mode=return_mode)
     # Load dataset
     raw_data = load_msft(parameters)
     raw_data = select_data_time(raw_data, parameters.start_date, parameters.end_date)
     # Plot
+    title = "Abs returns from adj. closing price"  # "One-day returns from adjusted closing stock price"
     plot_data(raw_data,
-              "Return",
+              parameters.target_label,
               "Date",
               plot_format="b",
-              title="One-day returns from adjusted closing stock price",
+              title=title,
               mode="Standard",
               tick_interval_x=parameters.tick_interval_x)
 
 
+def plot_return_full(return_mode="standard"):
+    """
+    All plots, complete timeframe
 
-def plot_return_full():
+    :param return_mode: "If "standard", the signed returns are used. If "abs" absolute values of returns are used"
+    :type return_mode: str
+
+    :return: ---
+    :rtype: None
+    """
+
     # Parameters
     parameters = Parameters(start_date="1980-01-01",
                             end_date="2024-12-31",
                             tick_interval_x=1000,
                             use_return=True,
                             prediction_horizon=10,
-                            target_label="Adj Close")
+                            target_label="Adj Close",
+                            return_mode=return_mode)
     # Load dataset
     raw_data = load_msft(parameters)
     raw_data = select_data_time(raw_data, parameters.start_date, parameters.end_date)
     # Plot
     plot_data(raw_data,
-              "Return",
+              parameters.target_label,
               "Date",
               plot_format="b",
               title="",
@@ -70,30 +92,64 @@ def plot_return_full():
               tick_interval_x=parameters.tick_interval_x,
               nlag_acf=360)
 
-def plot_return_full_subs():
+
+def plot_return_full_subs(return_mode="standard"):
+    """
+    All plots, two subset timeframes
+
+    :param return_mode: "If "standard", the signed returns are used. If "abs" absolute values of returns are used"
+    :type return_mode: str
+
+    :return: ---
+    :rtype: None
+    """
+
     # Parameters
     parameters_low = Parameters(start_date="1993-01-01",
                                 end_date="1995-12-31",
                                 tick_interval_x=1000,
                                 use_return=True,
                                 prediction_horizon=10,
-                                target_label="Adj Close")
+                                target_label="Adj Close",
+                                return_mode=return_mode)
+
+    parameters_low_2 = Parameters(start_date="2011-01-01",
+                                  end_date="2012-12-31",
+                                  tick_interval_x=1000,
+                                  use_return=True,
+                                  prediction_horizon=10,
+                                  target_label="Adj Close",
+                                  return_mode=return_mode)
 
     parameters_high = Parameters(start_date="2000-01-01",
                                  end_date="2003-12-31",
                                  tick_interval_x=1000,
                                  use_return=True,
                                  prediction_horizon=10,
-                                 target_label="Adj Close")
+                                 target_label="Adj Close",
+                                 return_mode=return_mode)
+
+    parameters_high_2 = Parameters(start_date="2008-01-01",
+                                   end_date="2009-12-31",
+                                   tick_interval_x=1000,
+                                   use_return=True,
+                                   prediction_horizon=10,
+                                   target_label="Adj Close",
+                                   return_mode=return_mode)
 
     # Load dataset
     raw_data = load_msft(parameters_low)
+    parameters_low_2.target_label = parameters_low.target_label
+    parameters_high.target_label = parameters_low.target_label
+    parameters_high_2.target_label = parameters_low.target_label
     data_subs_low = select_data_time(raw_data, parameters_low.start_date, parameters_low.end_date)
+    data_subs_low_2 = select_data_time(raw_data, parameters_low_2.start_date, parameters_low_2.end_date)
     data_subs_high = select_data_time(raw_data, parameters_high.start_date, parameters_high.end_date)
+    data_subs_high_2 = select_data_time(raw_data, parameters_high_2.start_date, parameters_high_2.end_date)
 
     # Plot
     plot_data(data_subs_low,
-              "Return",
+              parameters_low.target_label,
               "Date",
               plot_format="b",
               title="",
@@ -101,14 +157,33 @@ def plot_return_full_subs():
               tick_interval_x=parameters_low.tick_interval_x,
               nlag_acf=360)
 
+    plot_data(data_subs_low_2,
+              parameters_low_2.target_label,
+              "Date",
+              plot_format="b",
+              title="",
+              mode="Full",
+              tick_interval_x=parameters_low_2.tick_interval_x,
+              nlag_acf=360)
+
     plot_data(data_subs_high,
-              "Return",
+              parameters_high.target_label,
               "Date",
               plot_format="b",
               title="",
               mode="Full",
               tick_interval_x=parameters_high.tick_interval_x,
               nlag_acf=360)
+
+    plot_data(data_subs_high_2,
+              parameters_high_2.target_label,
+              "Date",
+              plot_format="b",
+              title="",
+              mode="Full",
+              tick_interval_x=parameters_high_2.tick_interval_x,
+              nlag_acf=360)
+
 
 def make_arg_parser():
     """
@@ -119,8 +194,10 @@ def make_arg_parser():
     """
 
     parser = ArgumentParser()
-    parser.add_argument("--mode", type=str, help="Mode: init_example/plot_return_ts/plot_return_full/plot_return_full_subs", required=False)
-
+    parser.add_argument("--mode", type=str,
+                        help="Mode: init_example/plot_return_ts/plot_return_full/plot_return_full_subs", required=False)
+    parser.add_argument("--return_mode", type=str,
+                        help="If not set or set to standard, returns are used. If set to abs, abs returns are used")
     return parser
 
 
@@ -135,14 +212,19 @@ def main():
     else:
         mode = args.mode
 
+    if args.return_mode is None:
+        return_mode = "standard"
+    else:
+        return_mode = args.return_mode
+
     if mode == "init_example":
         run_initial_example()
     elif mode == "plot_return_ts":
-        plot_return_ts()
+        plot_return_ts(return_mode=return_mode)
     elif mode == "plot_return_full":
-        plot_return_full()
+        plot_return_full(return_mode=return_mode)
     elif mode == "plot_return_full_subs":
-        plot_return_full_subs()
+        plot_return_full_subs(return_mode=return_mode)
     else:
         print("Invalid mode")
 
