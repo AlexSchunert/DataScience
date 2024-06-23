ee2978bd28270f99cf65179913236006731a4284
AlexSchunert <a.schunert@gmail.com>
2024-06-23 21:09:35 +0200

    Removed requirement for Date field in gp fit, moved fit acf to new file

--- MSFT_GP/utils.py
+++ MSFT_GP/utils.py
@@ -1,9 +1,9 @@
+import pandas
 import pandas as pd
 from sklearn.model_selection import train_test_split as train_test_split_sklearn
 from dataclasses import dataclass
 from numpy import ndarray, zeros, abs, exp as np_exp, sin
 from scipy.signal import correlate
-from scipy.optimize import curve_fit
 from kernel_functions import rbf_kernel
 
 
@@ -245,12 +245,20 @@ def construct_prediction_result(data, mu_predicted, std_predicted, result_label=
     :rtype: pd.DateFrame
     """
     # Create result series
-    result = pd.DataFrame({
-        "Date": data["Date"].reset_index(drop=True),
-        "dt": data["dt"].reset_index(drop=True),
-        result_label: pd.Series(mu_predicted.reshape(-1)),
-        "std": pd.Series(std_predicted)
-    })
+    if "Date" in data.columns:
+        result = pd.DataFrame({
+            "Date": data["Date"].reset_index(drop=True),
+            "dt": data["dt"].reset_index(drop=True),
+            result_label: pd.Series(mu_predicted.reshape(-1)),
+            "std": pd.Series(std_predicted)
+        })
+    else:
+        result = pd.DataFrame({
+            "dt": data["dt"].reset_index(drop=True),
+            result_label: pd.Series(mu_predicted.reshape(-1)),
+            "std": pd.Series(std_predicted)
+        })
+
     return result
 
 
@@ -296,37 +304,3 @@ def autocorrelations_sliding_window(time_series, window_size):
         autocorrelations[i, :] = autocorr
 
     return autocorrelations
-
-
-def fit_acf(dt, correlation):
-    """
-    Fit functions to acf to determine hyperparameters of kernel functions
-
-    :param dt: The lag (=> x)
-    :type dt: ndarray
-    :param correlation: The autocorrelation (=> y)
-    :type correlation: ndarray
-
-    :return: Tuple of (Array of optimized function parameters, Function values for dt)
-    :rtype: (ndarray,ndarray)
-    """
-
-    # Exponential
-    #acf_model = lambda x, x0, l: np_exp(-abs(x-x0) / l)
-    #fit_result = curve_fit(acf_model, dt, correlation, p0=(0.0, 10.0))
-    #pfit_opt = fit_result[0]
-    #fct_values = acf_model(dt, pfit_opt[0],pfit_opt[1])
-
-    # Exponential + periodic
-    acf_model = lambda x, x0, l, A, f, phi0: np_exp(-abs(x - x0) / l) + A * sin(f*x + phi0)
-    fit_result = curve_fit(acf_model, dt, correlation, p0=(0.0, 10.0, 0.1, 0.01, 0.0))
-    pfit_opt = fit_result[0]
-    fct_values = acf_model(dt, pfit_opt[0], pfit_opt[1], pfit_opt[2], pfit_opt[3], pfit_opt[4])
-    print(pfit_opt)
-    # hyperbolic
-    # acf_model = lambda x, A, x0, y0: A / abs(x + x0) + y0
-    # fit_result = curve_fit(acf_model, dt, correlation, p0=(1.0, 1.0, 0.0))
-    # pfit_opt = fit_result[0]
-    # fct_values = acf_model(dt, pfit_opt[0], pfit_opt[1], pfit_opt[2])
-
-    return pfit_opt, fct_values
