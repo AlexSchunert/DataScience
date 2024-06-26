7e74bb6206f564f10e2415552e2535e80481c938
AlexSchunert <a.schunert@gmail.com>
2024-06-26 19:42:54 +0200

    moved GPPosterior to gp_utils

--- MSFT_GP/gaussian_process.py
+++ MSFT_GP/gaussian_process.py
@@ -3,33 +3,9 @@ import pandas as pd
 from dataclasses import dataclass
 from utils import construct_prediction_result
 from kernel_functions import rbf_kernel
+from gp_utils import GPPosterior
 
 
-@dataclass
-class GPPosterior:
-    """
-    Contains the posterior of a gp given by represented weights and predictive covariance
-    """
-
-    repr_weights: ndarray = empty,
-    predictive_cov: ndarray = empty
-
-    def __int__(self,
-                repr_weights,
-                predictive_cov):
-        """
-        Inits GPPosterior
-
-        :param repr_weights: Representer weights determined during conditioning
-        :type repr_weights: ndarray
-        :param predictive_cov: Predictive covariance determined during conditioning
-        :type predictive_cov: ndarray
-
-        :return: ---
-        :rtype: None
-        """
-        self.repr_weights = repr_weights
-        self.predictive_cov = predictive_cov
 
 
 def compute_kernel_matrices(predict_data,
