e57c63ee217aa13931decee0df2c2096429f0836
AlexSchunert <a.schunert@gmail.com>
2024-06-26 19:44:42 +0200

    Added part of gp-kernel implementation

--- MSFT_GP/kernel_functions.py
+++ MSFT_GP/kernel_functions.py
@@ -1,6 +1,7 @@
 from sklearn.metrics.pairwise import rbf_kernel as rbf_kernel_sklearn
 from pandas import DataFrame
 from numpy import ndarray
+from gp_utils import GPPosterior
 
 # Todo: Remove "dt"
 def rbf_kernel(input_left, input_right, length_scale=10.0, output_scale=15.0):
@@ -38,3 +39,31 @@ def rbf_kernel(input_left, input_right, length_scale=10.0, output_scale=15.0):
     k_ab = output_scale * rbf_kernel_sklearn(a, b, gamma=rbf_gamma)
 
     return k_ab
+
+def gp_kernel(input_left, input_right, gp_posterior):
+    """
+    Calculates the values of kernel estimated from acf fit with gp
+
+    :param input_left: DataFrame containing input a. Must have column labeled "dt"
+    :type input_left: DataFrame
+    :param input_right: DataFrame containing input b. Must have column labeled "dt"
+    :type input_right: DataFrame
+    :param gp_posterior: Struct containing at least representer weights and predictive covariance
+    :type gp_posterior: GPPosterior
+
+    :return: Kernel matrix of inputs
+    :rtype: ndarray
+    """
+    if input_left["dt"].values.ndim == 1:
+        a = input_left["dt"].values.reshape(-1, 1)
+    elif input_left["dt"].values.ndim == 2:
+        a = input_left["dt"].values
+    else:
+        return
+
+    if input_right["dt"].values.ndim == 1:
+        b = input_right["dt"].values.reshape(-1, 1)
+    elif input_right["dt"].values.ndim == 2:
+        b = input_right["dt"].values
+    else:
+        return
