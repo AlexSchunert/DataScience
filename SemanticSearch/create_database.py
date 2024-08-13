90de342ed4fb2293f2ea0dcda1bd9ff77e553ac4
AlexSchunert <a.schunert@gmail.com>
2024-08-13 19:54:42 +0200

    Add ids to text chunks to avoid duplicates in db

--- SemanticSearch/create_database.py
+++ SemanticSearch/create_database.py
@@ -1,13 +1,24 @@
-from typing import List
+from typing import List, Dict
 from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents.base import Document
 from langchain_community.vectorstores import Chroma as ChromaVecStore
 from langchain_huggingface import HuggingFaceEmbeddings
 from chromadb import PersistentClient
+from dataclasses import dataclass
 from os.path import exists
 from shutil import rmtree
 from yaml import safe_load as safe_load_yaml
+
+
+def add_chunk_ids(chunks: List[Document]) -> List[Dict]:
+    chunks_with_ids = []
+    for chunk in chunks:
+        unique_id = chunk.metadata["source"] + str(chunk.metadata["page"]) + str(chunk.metadata["start_index"])
+        chunks_with_ids.append({"id": unique_id, "chunk": chunk})
+
+    return chunks_with_ids
+
 
 def load_docs(doc_path: str, chunk_size: int = 1000, chunk_overlap: int = 500) -> List[Document]:
     """
@@ -26,13 +37,14 @@ def load_docs(doc_path: str, chunk_size: int = 1000, chunk_overlap: int = 500) -
                                                    add_start_index=True, )
     docs = loader.load()
     chunks = text_splitter.split_documents(docs)
+    # Add IDs
+    chunks_with_ids = add_chunk_ids(chunks)
+
     print(f"Split {len(docs)} docs into {len(chunks)} chunks")
     return chunks
 
 
-def save_to_chroma(chunks: list[Document], chroma_path: str = "chroma", collection_name: str ="my_collection") -> None:
-
-
+def save_to_chroma(chunks: list[Document], chroma_path: str = "chroma", collection_name: str = "my_collection") -> None:
     # Init embedding function
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
     model_kwargs = {'device': 'cpu'}
@@ -45,12 +57,13 @@ def save_to_chroma(chunks: list[Document], chroma_path: str = "chroma", collecti
 
     # Create or load chromaDB
     client = PersistentClient(path=chroma_path)
-    #collection = client.get_or_create_collection(name=collection_name)
+    # collection = client.get_or_create_collection(name=collection_name)
 
     vector_store = ChromaVecStore(client=client, embedding_function=embeddings, collection_name=collection_name)
 
+    # Remove already
+
     # Add data
     vector_store.add_documents(chunks)
-
 
     print(f"Saved {len(chunks)} chunks to {chroma_path}.")
