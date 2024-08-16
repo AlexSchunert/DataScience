36ddf9c943e368c0641249f8cbde1a10841dbed7
AlexSchunert <a.schunert@gmail.com>
2024-08-16 20:40:53 +0200

    Add calculation of sha256 id for each doc, use ids in chromadb to avoid duplicates, add doc strings

--- SemanticSearch/create_database.py
+++ SemanticSearch/create_database.py
@@ -5,19 +5,32 @@ from langchain_core.documents.base import Document
 from langchain_community.vectorstores import Chroma as ChromaVecStore
 from langchain_huggingface import HuggingFaceEmbeddings
 from chromadb import PersistentClient
+from hashlib import sha256
+from pathlib import Path
 from dataclasses import dataclass
-from os.path import exists
+from os.path import getmtime
 from shutil import rmtree
 from yaml import safe_load as safe_load_yaml
 
 
-def add_chunk_ids(chunks: List[Document]) -> List[Dict]:
-    chunks_with_ids = []
-    for chunk in chunks:
-        unique_id = chunk.metadata["source"] + str(chunk.metadata["page"]) + str(chunk.metadata["start_index"])
-        chunks_with_ids.append({"id": unique_id, "chunk": chunk})
+def add_chunk_ids(chunks: List[Document]) -> List[Document]:
+    """
+    Adds sha256 to input chunks
 
-    return chunks_with_ids
+    :param param1: Input chunks
+
+    :return: Chunks with ids
+    """
+
+    for chunk in chunks:
+        #unique_id = chunk.metadata["source"] + str(chunk.metadata["page"]) + str(chunk.metadata["start_index"])
+        hash_obj = sha256()
+        hash_obj.update(chunk.page_content.encode())
+        #unique_id = hash_obj.hexdigest()
+        chunk.id = hash_obj.hexdigest()
+
+
+    return chunks
 
 
 def load_docs(doc_path: str, chunk_size: int = 1000, chunk_overlap: int = 500) -> List[Document]:
@@ -38,13 +51,23 @@ def load_docs(doc_path: str, chunk_size: int = 1000, chunk_overlap: int = 500) -
     docs = loader.load()
     chunks = text_splitter.split_documents(docs)
     # Add IDs
-    chunks_with_ids = add_chunk_ids(chunks)
+    chunks = add_chunk_ids(chunks)
 
     print(f"Split {len(docs)} docs into {len(chunks)} chunks")
     return chunks
 
 
 def save_to_chroma(chunks: list[Document], chroma_path: str = "chroma", collection_name: str = "my_collection") -> None:
+    """
+    Saves list of documents in chunks to chromadb saved at path chroma_path.
+
+    :param chunks: List of documents to be added
+    :param chroma_path: Path to persistent chroma-db
+    :param collection_name: Name of collection to used. Created in case it does not exist
+
+    :return: None
+    """
+
     # Init embedding function
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
     model_kwargs = {'device': 'cpu'}
@@ -58,12 +81,15 @@ def save_to_chroma(chunks: list[Document], chroma_path: str = "chroma", collecti
     # Create or load chromaDB
     client = PersistentClient(path=chroma_path)
     # collection = client.get_or_create_collection(name=collection_name)
-
     vector_store = ChromaVecStore(client=client, embedding_function=embeddings, collection_name=collection_name)
 
-    # Remove already
-
     # Add data
-    vector_store.add_documents(chunks)
+    ids = [chunk.id for chunk in chunks]
 
-    print(f"Saved {len(chunks)} chunks to {chroma_path}.")
+    num_elems_pre = client.get_collection(name=collection_name).count()
+    print(f"#Entries in {collection_name}: {num_elems_pre} before insert")
+    vector_store.add_documents(chunks, ids=ids)
+    num_elems_post = client.get_collection(name=collection_name).count()
+    print(f"#Entries in {collection_name}: {client.get_collection(name=collection_name).count()} after insert")
+    print(f"Added {num_elems_post - num_elems_pre} elements")
+
