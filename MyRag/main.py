13294c9cb2a2c91e24aa60918bdb7a4bf7e18bed
AlexSchunert <a.schunert@gmail.com>
2024-08-18 12:29:44 +0200

    Get parameters from yaml file, make db embedding a paramter

--- MyRag/main.py
+++ MyRag/main.py
@@ -1,29 +1,48 @@
-import fitz  # PyMuPDF
+from yaml import safe_load
+from typing import Dict
 from vector_database import index_directory, query_database
 
 
+def load_config(config_file: str = "config.yaml") -> Dict:
+    """
+    Loads yaml config and returns a dict
+
+    :param config_file: Path to config file
+
+
+    :return: The config object
+    """
+
+    with open(config_file, "r") as config_file:
+        config = safe_load(config_file)
+
+    return config
+
+
 def query_db_main():
-    chroma_path = "./chroma"
-    collection_name = "my_collection"
+    config = load_config()
+    chroma_path = config["default_chroma_path"]
+    collection_name = config["default_collection_name"]
     query = "What is science"
+
     query_database(chroma_path,
                    collection_name,
                    query)
 
 
 def index_dir_main():
+    config = load_config()
     doc_path = "../DataSets"
-    chunk_size = 600
-    chunk_overlap = 300
-    chroma_path = "./chroma"
-    collection_name = "my_collection"
+    chunk_size = config["default_chunk_size"]
+    chunk_overlap = config["default_chunk_overlap"]
+    chroma_path =  config["default_chroma_path"]
+    collection_name = config["default_collection_name"]
 
     index_directory(doc_path,
                     chunk_size=chunk_size,
                     chunk_overlap=chunk_overlap,
                     chroma_path=chroma_path,
                     collection_name=collection_name)
-
 
 
 if __name__ == '__main__':
--- MyRag/vector_database.py
+++ MyRag/vector_database.py
@@ -39,6 +39,7 @@ def query_database(chroma_path: str = "./chroma",
 
 
 def index_directory(doc_path: str,
+                    embedding_type: str = "sentence-transformers/all-MiniLM-L6-v2",
                     chunk_size: int = 1000,
                     chunk_overlap: int = 500,
                     chroma_path: str = "chroma",
@@ -46,9 +47,10 @@ def index_directory(doc_path: str,
     """
     Parses all files of a directory (currenltly only pdf supported). Indexes and adds them to a chroma database.
 
-    :param doc_path: path to folder to be indexed
-    :param chunk_size: size of produced text chunks
-    :param chunk_overlap: overlap between produced text chunks
+    :param doc_path: Path to folder to be indexed
+    :param embedding_type: Type of embedding to be used. Must be in HuggingFaceEmbeddings model_name(s)
+    :param chunk_size: Size of produced text chunks
+    :param chunk_overlap: Overlap between produced text chunks
     :param chroma_path: Path to persistent chroma-db
     :param collection_name: Name of collection to used. Created in case it does not exist
 
@@ -56,14 +58,18 @@ def index_directory(doc_path: str,
     """
 
     chunks = parse_folder_to_doc(doc_path, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    save_to_chroma(chunks, chroma_path=chroma_path, collection_name=collection_name)
+    save_to_chroma(chunks, embedding_type=embedding_type, chroma_path=chroma_path, collection_name=collection_name)
 
 
-def save_to_chroma(chunks: List[Document], chroma_path: str = "chroma", collection_name: str = "my_collection") -> None:
+def save_to_chroma(chunks: List[Document],
+                   embedding_type: str = "sentence-transformers/all-MiniLM-L6-v2",
+                   chroma_path: str = "chroma",
+                   collection_name: str = "my_collection") -> None:
     """
     Saves list of documents in chunks to chromadb saved at path chroma_path.
 
     :param chunks: List of documents to be added
+    :param embedding_type: Type of embedding to be used. Must be in HuggingFaceEmbeddings model_name(s)
     :param chroma_path: Path to persistent chroma-db
     :param collection_name: Name of collection to used. Created in case it does not exist
 
@@ -71,7 +77,7 @@ def save_to_chroma(chunks: List[Document], chroma_path: str = "chroma", collecti
     """
 
     # Init embedding function
-    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    model_name = embedding_type #"sentence-transformers/all-MiniLM-L6-v2"
     model_kwargs = {'device': 'cpu'}
     encode_kwargs = {'normalize_embeddings': False}
     embeddings = HuggingFaceEmbeddings(
