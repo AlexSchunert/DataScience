429c44fd4b33f9ab9aef574ffa8e63ec7bb978b3
AlexSchunert <a.schunert@gmail.com>
2024-08-25 20:17:01 +0200

    Add tag-/attribute-name based filtering of html content, add method to find all links on a web-page

--- MyScraper/MyScraper.py
+++ MyScraper/MyScraper.py
@@ -3,8 +3,19 @@ from bs4 import BeautifulSoup
 from bs4.element import ResultSet
 from urllib.robotparser import RobotFileParser
 from urllib.parse import urlparse, urljoin
-from typing import Optional
+from typing import Optional, Dict, List
 from time import time, sleep
+from dataclasses import dataclass
+
+
+@dataclass
+class HTMLTagCondition:
+    tag_condition: str = None
+    attr_conditions: Dict = None
+
+    def ____init__(self, tag_condition: str = None, attr_conditions: Dict = None):
+        self.tag_condition = tag_condition
+        self.attr_conditions = attr_conditions
 
 
 class MyScraper:
@@ -12,8 +23,8 @@ class MyScraper:
     user_agent: str  # The User-Agent string for web scraping
     soup: Optional[BeautifulSoup]  # BeautifulSoup object for HTML parsing
     robots_parser: RobotFileParser  # RobotFileParser object to handle robots.txt rules
-    time_last_request: float # TBD
-    wait_time_min_s: float # TBD
+    time_last_request: float  # TBD
+    wait_time_min_s: float  # TBD
 
     def __init__(self, base_url: str, user_agent: str = '*', wait_time_min_s: float = 1.0) -> None:
 
@@ -35,7 +46,23 @@ class MyScraper:
         self.wait_time_min_s = wait_time_min_s
         pass
 
-    def fetch_website_content(self, url: str):
+    def extract_links(self):
+        links = []
+        for a_tag in self.soup.find_all("a", href=True):
+            href = a_tag["href"]
+            # Ensure link is absolute
+            full_url = urljoin(self.base_url, href)
+            # Filter internal links only
+            if self.is_internal_link(full_url):
+                links.append(full_url)
+
+        return links
+
+    def is_internal_link(self, url):
+        """Check if a link is internal (belongs to the same domain)."""
+        return urlparse(url).netloc == urlparse(self.base_url).netloc
+
+    def fetch_webpage(self, url: str):
         if not self.is_allowed(url):
             print(f"Reading prohibited: {url}")
 
@@ -49,12 +76,22 @@ class MyScraper:
                 print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
         pass
 
-    def query_soup_tag(self, tag_name: str):
-        return self.soup.find_all(tag_name)
+    def prettify_webpage(self) -> str:
+        return self.soup.prettify()
 
-    def query_soup_attr(self, attr_name: str, attr_value: str) -> ResultSet:
-        return self.soup.find_all(attrs={attr_name: attr_value})
+    def filter_webpage(self, filter_conditions: List[HTMLTagCondition]) -> ResultSet:
 
+        result = ResultSet([], [])
+        for condition in filter_conditions:
+            if condition.tag_condition and condition.attr_conditions:
+                result += self.soup.find_all(condition.tag_condition, attrs=condition.attr_conditions)
+            elif condition.tag_condition and not condition.attr_conditions:
+                result += self.soup.find_all(condition.tag_condition)
+            elif not condition.tag_condition and condition.attr_conditions:
+                result += self.soup.find_all(attrs=condition.attr_conditions)
+            else:
+                print("Invalid tag and attribute condition")
+        return result
 
     def wait_for_timeout(self) -> None:
         current_time = time()
