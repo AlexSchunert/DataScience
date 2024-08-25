a0522ce4a9aacef7391597a5fca922914777abda
AlexSchunert <a.schunert@gmail.com>
2024-08-25 20:17:01 +0200

    Add tag-/attribute-name based filtering of html content, add method to find all links on a web-page

--- MyScraper/main.py
+++ MyScraper/main.py
@@ -1,19 +1,25 @@
-from MyScraper import MyScraper
+from MyScraper import MyScraper, HTMLTagCondition
 
 
 def main(scrape_url_base: str):
-
     my_scraper = MyScraper(base_url)
-    my_scraper.fetch_website_content(base_url)
-    result = my_scraper.query_soup_tag("span")
-    result = my_scraper.query_soup_attr(attr_name="class", attr_value="text")
+    my_scraper.fetch_webpage(base_url)
+    # result = my_scraper.query_soup_tag("span")
+    # result = my_scraper.query_soup_attr(attr_name="class", attr_value="text")
+    filter_conditions = []
+    #cond1 = HTMLTagCondition(tag_condition="small")
+    #cond2 = HTMLTagCondition(tag_condition="span", attr_conditions={"class": "text"})
+    #filter_conditions.append(cond1)
+    #filter_conditions.append(cond2)
+    cond1 = HTMLTagCondition(tag_condition="div", attr_conditions={"class": "quote"})
+    filter_conditions.append(cond1)
+    filtered_tags = my_scraper.filter_webpage(filter_conditions)
+    links = my_scraper.extract_links()
 
     pass
 
 
-
-
 if __name__ == '__main__':
-    #base_url = "https://www.scrapethissite.com/"
+    # base_url = "https://www.scrapethissite.com/"
     base_url = "https://quotes.toscrape.com/"
     main(base_url)
