f85d1d01cf3c47dd1a2d54e4f88012687da80a2e
AlexSchunert <a.schunert@gmail.com>
2024-08-27 21:10:00 +0200

    Add basic website traversal

--- MyScraper/main.py
+++ MyScraper/main.py
@@ -2,8 +2,8 @@ from MyScraper import MyScraper, HTMLTagCondition
 
 
 def main(scrape_url_base: str):
-    my_scraper = MyScraper(base_url)
-    my_scraper.fetch_webpage(base_url)
+    my_scraper = MyScraper(base_url, wait_time_min_s=0.1)
+    #my_scraper.fetch_webpage(base_url)
     # result = my_scraper.query_soup_tag("span")
     # result = my_scraper.query_soup_attr(attr_name="class", attr_value="text")
     filter_conditions = []
@@ -11,11 +11,11 @@ def main(scrape_url_base: str):
     #cond2 = HTMLTagCondition(tag_condition="span", attr_conditions={"class": "text"})
     #filter_conditions.append(cond1)
     #filter_conditions.append(cond2)
-    cond1 = HTMLTagCondition(tag_condition="div", attr_conditions={"class": "quote"})
-    filter_conditions.append(cond1)
-    filtered_tags = my_scraper.filter_webpage(filter_conditions)
-    links = my_scraper.extract_links()
-
+    #cond1 = HTMLTagCondition(tag_condition="div", attr_conditions={"class": "quote"})
+    #filter_conditions.append(cond1)
+    #filtered_tags = my_scraper.filter_webpage(filter_conditions)
+    #links = my_scraper.extract_links_from_website()
+    my_scraper.traverse_website()
     pass
 
 
