024c2aa6a0452db76f1398d4fc70eafd3de171ab
AlexSchunert <a.schunert@gmail.com>
2024-08-28 20:54:09 +0200

    Add result type for website traversal, add regexp filter for url in website traversal, enfore uniqueness of html content in returned filtered_tags

--- MyScraper/MyScraper.py
+++ MyScraper/MyScraper.py
@@ -7,7 +7,16 @@ from urllib.parse import urlparse, urljoin
 from typing import Optional, Dict, List
 from time import time, sleep
 from dataclasses import dataclass
+from re import search
 
+@dataclass
+class HTMLTag:
+    html_content: str
+    metadata: Dict
+
+    def __init__(self, html_content: str = "", source: str = ""):
+        self.metadata = {"source": source}
+        self.html_content = html_content
 
 @dataclass
 class HTMLTagCondition:
@@ -27,8 +36,17 @@ class MyScraper:
     time_last_request: float  # TBD
     wait_time_min_s: float  # TBD
     webpage_index: Dict  #
+    link_search_pattern: str
+    filter_conditions: List[HTMLTagCondition]
+    filtered_tags: List[HTMLTag]
+    current_url: str
 
-    def __init__(self, base_url: str, user_agent: str = '*', wait_time_min_s: float = 1.0) -> None:
+    def __init__(self,
+                 base_url: str,
+                 user_agent: str = '*',
+                 wait_time_min_s: float = 1.0,
+                 link_search_pattern: str = r"",
+                 filter_conditions: List[HTMLTagCondition] = None) -> None:
 
         # Initialize the base URL and user agent
         self.base_url = base_url
@@ -50,6 +68,19 @@ class MyScraper:
         # Initialize webpage_index
         self.webpage_index = {self.base_url: False}
 
+        # Initialize html tag filter results
+        self.filtered_tags = ResultSet([], [])
+
+        # Initialize search conditions
+        self.link_search_pattern = link_search_pattern
+        if filter_conditions is None:
+            self.filter_conditions = [HTMLTagCondition()]
+        else:
+            self.filter_conditions = filter_conditions
+
+        # Initialize helper
+        self.current_url = self.base_url
+
     def extract_links_from_website(self) -> List[str]:
         links = []
         for a_tag in self.soup.find_all("a", href=True):
@@ -57,7 +88,7 @@ class MyScraper:
             # Ensure link is absolute
             full_url = urljoin(self.base_url, href)
             # Filter internal links only
-            if self.is_internal_link(full_url):
+            if self.is_internal_link(full_url) and search(self.link_search_pattern, full_url) is not None:
                 links.append(full_url)
 
         return links
@@ -76,6 +107,7 @@ class MyScraper:
             self.time_last_request = time()
             if response.status_code == 200:
                 self.soup = BeautifulSoup(response.text, 'html.parser')
+                self.current_url = url
             else:
                 print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
         pass
@@ -83,19 +115,26 @@ class MyScraper:
     def prettify_webpage(self) -> str:
         return self.soup.prettify()
 
-    def filter_webpage(self, filter_conditions: List[HTMLTagCondition]) -> ResultSet:
+    def filter_webpage(self) -> None:
 
-        result = ResultSet([], [])
-        for condition in filter_conditions:
+        for condition in self.filter_conditions:
             if condition.tag_condition and condition.attr_conditions:
-                result += self.soup.find_all(condition.tag_condition, attrs=condition.attr_conditions)
+                search_result = self.soup.find_all(condition.tag_condition, attrs=condition.attr_conditions)
+                self.filtered_tags.extend([HTMLTag(html_content=tag.prettify(),
+                                                   source=self.current_url) for tag in search_result])
+
             elif condition.tag_condition and not condition.attr_conditions:
-                result += self.soup.find_all(condition.tag_condition)
+                search_result = self.soup.find_all(condition.tag_condition)
+                self.filtered_tags.extend([HTMLTag(html_content=tag.prettify(),
+                                                   source=self.current_url) for tag in search_result])
             elif not condition.tag_condition and condition.attr_conditions:
-                result += self.soup.find_all(attrs=condition.attr_conditions)
+                search_result = self.soup.find_all(attrs=condition.attr_conditions)
+                self.filtered_tags.extend([HTMLTag(html_content=tag.prettify(),
+                                                   source=self.current_url) for tag in search_result])
             else:
-                print("Invalid tag and attribute condition")
-        return result
+                search_result = self.soup.find(True)
+                self.filtered_tags.extend([HTMLTag(html_content=tag.prettify(),
+                                                   source=self.current_url) for tag in search_result])
 
     def wait_for_timeout(self) -> None:
         current_time = time()
@@ -117,14 +156,15 @@ class MyScraper:
             self.fetch_webpage(current_url)
             website_links = self.extract_links_from_website()
 
-            #for link in website_links:
+            # for link in website_links:
             #    if link not in self.webpage_index:
             #        self.webpage_index[link] = False
             self.webpage_index = {s: False for s in website_links} | self.webpage_index
-
+            self.filter_webpage()
             self.webpage_index[current_url] = True
             idx_first_unvisited = self.find_unvisited_webpage()
-            print(f"Finished Parsing {current_url}. Progress: {sum(list(self.webpage_index.values()))}/{len(self.webpage_index)}")
+            print(
+                f"Finished Parsing {current_url}. Progress: {sum(list(self.webpage_index.values()))}/{len(self.webpage_index)}")
 
     def find_unvisited_webpage(self) -> Optional[int]:
 
@@ -135,3 +175,12 @@ class MyScraper:
             print("No unvisited webpage found -> Done")
 
         return idx_first_unvisited
+
+    def return_filtered_tags(self, remove_duplicates: bool = True) -> List[HTMLTag]:
+
+        if remove_duplicates:
+            _, unique_idxs = np.unique([tag.html_content for tag in self.filtered_tags], return_index=True)
+            return [self.filtered_tags[idx] for idx in unique_idxs]
+        else:
+            return self.filtered_tags
+
--- MyScraper/main.py
+++ MyScraper/main.py
@@ -2,20 +2,34 @@ from MyScraper import MyScraper, HTMLTagCondition
 
 
 def main(scrape_url_base: str):
-    my_scraper = MyScraper(base_url, wait_time_min_s=0.1)
-    #my_scraper.fetch_webpage(base_url)
+    # Create hmtl filter conditions
+    filter_conditions = []
+    cond1 = HTMLTagCondition(tag_condition="div", attr_conditions={"class": "quote"})
+    filter_conditions.append(cond1)
+    # Create link
+    link_search_pattern = r"https://quotes.toscrape.com/page/\d/"
+
+    my_scraper = MyScraper(base_url,
+                           wait_time_min_s=0.5,
+                           filter_conditions=filter_conditions,
+                           link_search_pattern=link_search_pattern)
+
+    my_scraper.traverse_website()
+    filtered_tags = my_scraper.return_filtered_tags()
+
+    # my_scraper.fetch_webpage(base_url)
     # result = my_scraper.query_soup_tag("span")
     # result = my_scraper.query_soup_attr(attr_name="class", attr_value="text")
-    filter_conditions = []
-    #cond1 = HTMLTagCondition(tag_condition="small")
-    #cond2 = HTMLTagCondition(tag_condition="span", attr_conditions={"class": "text"})
-    #filter_conditions.append(cond1)
-    #filter_conditions.append(cond2)
-    #cond1 = HTMLTagCondition(tag_condition="div", attr_conditions={"class": "quote"})
-    #filter_conditions.append(cond1)
-    #filtered_tags = my_scraper.filter_webpage(filter_conditions)
-    #links = my_scraper.extract_links_from_website()
-    my_scraper.traverse_website()
+    # filter_conditions = []
+    # cond1 = HTMLTagCondition(tag_condition="small")
+    # cond2 = HTMLTagCondition(tag_condition="span", attr_conditions={"class": "text"})
+    # filter_conditions.append(cond1)
+    # filter_conditions.append(cond2)
+    # cond1 = HTMLTagCondition(tag_condition="div", attr_conditions={"class": "quote"})
+    # filter_conditions.append(cond1)
+    # filtered_tags = my_scraper.filter_webpage(filter_conditions)
+    # links = my_scraper.extract_links_from_website()
+
     pass
 
 
